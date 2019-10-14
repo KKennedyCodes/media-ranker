1406e0b43e3a5bfc16a7d5ef4e56ef7d425ff9bb
KKennedyCodes <holmstedtk@gmail.com>
2019-10-14 15:25:38 -0700

    Added Validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+  has_many :votes
+  
+  valides :name, presence: true, uniqueness: true
+  
+  def total_votes
+  end
 end
--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,2 +1,13 @@
 class Vote < ApplicationRecord
+  belongs_to :user
+  belongs_to :work
+  
+  validates :user_id, presence: true;
+  validates :work_id, presence: true;
+  
+  def top_ten(media_type)
+  end
+  
+  def spotlight
+  end
 end
