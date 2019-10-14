61380f6c713dffcbe414ee8e12f4e793b3ed2d5b
KKennedyCodes <holmstedtk@gmail.com>
2019-10-14 15:55:50 -0700

    Created Seed File, Not Working, also Created User-Seeds data

--- db/schema.rb
+++ db/schema.rb
@@ -11,17 +11,17 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 2019_10_14_223759) do
-
+  
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
+  
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.datetime "join_date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-
+  
   create_table "votes", force: :cascade do |t|
     t.datetime "date"
     t.datetime "created_at", null: false
@@ -31,7 +31,7 @@ ActiveRecord::Schema.define(version: 2019_10_14_223759) do
     t.index ["user_id"], name: "index_votes_on_user_id"
     t.index ["work_id"], name: "index_votes_on_work_id"
   end
-
+  
   create_table "works", force: :cascade do |t|
     t.string "category"
     t.string "title"
@@ -41,7 +41,7 @@ ActiveRecord::Schema.define(version: 2019_10_14_223759) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-
+  
   add_foreign_key "votes", "users"
   add_foreign_key "votes", "works"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,58 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'csv'
+
+MEDIA_FILE = Rails.root.join('db', 'seed_data', 'media-seeds.csv')
+puts "Loading raw media data from #{MEDIA_FILE}"
+
+media_failures = []
+CSV.foreach(MEDIA_FILE, :headers => true) do |row|
+  media = Media.new
+  media.title = row['title']
+  media.category = row['category']
+  media.create = row['creator']
+  media.publication_year = row['publication_year']
+  media.description = row['description']
+  successful = media.save
+  
+  if !successful
+    media_failures << media
+    puts "Failed to save media: #{media.inspect}"
+  else
+    puts "Created media: #{media.inspect}"
+  end
+end
+
+puts "Added #{Media.count} media records"
+puts "#{media_failures.length} medias failed to save"
+
+
+
+USER_FILE = Rails.root.join('db', 'seed_data', 'user-seeds.csv')
+puts "Loading raw user data from #{USER_FILE}"
+
+user_failures = []
+CSV.foreach(USER_FILE, :headers => true) do |row|
+  user = User.new
+  user.name = row['name']
+  user.join_date = row['join_date']
+  successful = user.save
+  if !successful
+    user_failures << user
+    puts "Failed to save user: #{user.inspect}"
+  else
+    puts "Created user: #{user.inspect}"
+  end
+end
+
+puts "Added #{User.count} user records"
+puts "#{user_failures.length} users failed to save"
+
+# Since we set the primary key (the ID) manually on each of the
+# tables, we've got to tell postgres to reload the latest ID
+# values. Otherwise when we create a new record it will try
+# to start at ID 1, which will be a conflict.
+puts "Manually resetting PK sequence on each table"
+ActiveRecord::Base.connection.tables.each do |t|
+  ActiveRecord::Base.connection.reset_pk_sequence!(t)
+end
+
+puts "done"
