ded40002a3a64f2a09609c5900f1fa0462204af7
KKennedyCodes <holmstedtk@gmail.com>
2019-10-16 15:18:34 -0700

    Work Method Tests

--- test/models/work_test.rb
+++ test/models/work_test.rb
@@ -1,4 +1,53 @@
 require "test_helper"
 
 describe Work do
+  before do
+    @user = users(:kennedy)
+    @work = works(:pill)
+    @vote = votes(:pill_vote)
+  end
+  
+  describe "instantiations" do
+    it "can be instantiated" do
+      # Assert
+      expect(@work.valid?).must_equal true
+    end
+  end
+  
+  describe "validations" do
+    it "requies title to be instantiated" do
+      @work.title = nil
+      expect(@work.valid?).must_equal false
+      expect(@work.errors.messages).must_include :title   
+    end
+    
+    it "requies category to be instantiated" do
+      @work.category = nil
+      expect(@work.valid?).must_equal false
+      expect(@work.errors.messages).must_include :category   
+    end
+    
+    it "requies creator to be instantiated" do
+      @work.creator = nil
+      expect(@work.valid?).must_equal false
+      expect(@work.errors.messages).must_include :creator   
+    end
+    
+    it "requies publication year to be instantiated" do
+      @work.publication_year = nil
+      expect(@work.valid?).must_equal false
+      expect(@work.errors.messages).must_include :publication_year   
+    end
+  end
+  
+  # describe "relations" do
+  # end
+  
+  # describe "custom methods" do
+  #   # it "totals user works" do
+  #   #   work_total = total_works(@user.id)
+  #   #   expect(work_total).must_equal 2
+  #   # end
+  # end
 end
+
