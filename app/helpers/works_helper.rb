c109e77b61decba835fdfcbf670ac82833c7e552
KKennedyCodes <holmstedtk@gmail.com>
2019-10-16 11:23:59 -0700

    Added Application Helper Tests

--- app/helpers/works_helper.rb
+++ app/helpers/works_helper.rb
@@ -1,2 +1,8 @@
 module WorksHelper
+  def view_badge_adder(views)
+    return unless views > -1
+    return (
+    '<span class="badge badge-secondary">'.html_safe + views.to_s + '</span>'.html_safe
+    )
+  end
 end
