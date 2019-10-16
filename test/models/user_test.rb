6f501bd803da06488dfc08d76582e7be11ea088b
KKennedyCodes <holmstedtk@gmail.com>
2019-10-15 21:39:35 -0700

    Adding Info to Model Tests

---
+++ test/models/user_test.rb
@@ -0,0 +1,13 @@
+require "test_helper"
+
+describe User do
+  describe 'validations' do
+    # user = User.new(name: "Katie Kennedy", join_date: Time.now)
+    # it 'can be valid' do
+    #   is_valid = user.valid?
+    #   expect(is_valid).must_be_true
+    # end
+  end
+  
+  
+end
