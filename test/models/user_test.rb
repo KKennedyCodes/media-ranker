5d0214212d39c4572954484c02ede2e247c29241
KKennedyCodes <holmstedtk@gmail.com>
2019-10-15 21:51:19 -0700

    Still Configuring Model Tests - Not Working

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,13 +1,13 @@
 require "test_helper"
 
 describe User do
-  describe 'validations' do
-    # user = User.new(name: "Katie Kennedy", join_date: Time.now)
-    # it 'can be valid' do
-    #   is_valid = user.valid?
-    #   expect(is_valid).must_be_true
-    # end
-  end
-  
-  
+  let (:new_user) {
+  User.new(name: "Bob Bowers")
+}
+it "can be instantiated" do
+  # Assert
+  expect(new_user.valid?).must_equal true
+end
+
+
 end
