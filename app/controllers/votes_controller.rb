dd0bdefa1e62249b8a121b43c007b8f72d8b589f
KKennedyCodes <holmstedtk@gmail.com>
2019-10-17 16:35:10 -0700

    Vote Feature Added, Cannot Vote if Not Logged in, Cannot VOte more than oonce per work.

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -14,48 +14,67 @@ class VotesController < ApplicationController
   end
   
   def create
-    @vote = Vote.new(vote_params)
+    
+    generated_vote_params = {
+    date: Time.now, 
+    work_id: params[:work_id],
+    user_id: session[:user_id], 
+  }
+  
+  vote_status = Vote.where(work_id: generated_vote_params[:work_id])
+  vote_status_check = vote_status.where(user_id: generated_vote_params[:user_id])
+  
+  if vote_status_check.empty?
+    @vote = Vote.new(generated_vote_params)
+    
     if @vote.save
-      redirect_to votes_path
+      redirect_to works_path
       return
     else
-      render :new
+      #Active Record
+      #unique check and logged in check, inspect errors and display different flashes dependent on errors.
+      flash[:error] = "You have to be Logged in to Vote."
+      redirect_to works_path
       return
     end
+    
+  else
+    flash[:error] = "Vote has already been submitted."
+    redirect_to root_path
   end
-  
-  def edit; end
-  
-  def update
-    if @vote.update(vote_params)
-      redirect_to vote_path(@vote.id)
-      return
-    else
-      render user_path
-      return
-    end
+end
+def edit; end
+
+def update
+  if @vote.update(vote_params)
+    redirect_to vote_path(@vote.id)
+    return
+  else
+    render user_path
+    return
   end
-  
-  def destroy
-    @vote.destroy
-    redirect_to votes_path
+end
+
+def destroy
+  @vote.destroy
+  redirect_to votes_path
+end
+
+private
+
+def vote_params
+  return params.require(:vote).permit(:id, :work_id, :user_id, :date)
+end
+
+def find_vote
+  @vote = Vote.find_by(id: params[:id])
+end
+
+def vote_not_found
+  if @vote.nil?
+    flash[:error] = "Vote with ID: #{params[:id]} was not found."
+    redirect_to root_path
+    return
   end
-  
-  private
-  
-  def vote_params
-    return params.require(:vote).permit(:id, :work_id, :user_id, :date)
-  end
-  
-  def find_vote
-    @vote = Vote.find_by(id: params[:id])
-  end
-  
-  def vote_not_found
-    if @vote.nil?
-      flash[:error] = "Vote with ID: #{params[:id]} was not found."
-      redirect_to root_path
-      return
-    end
-  end
+end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,12 +4,13 @@ Rails.application.routes.draw do
   
   resources :works
   resources :users
-  resources :votes
+  #resources :votes
   
   get "/login", to: "users#login_form", as: "login"
   post "/login", to: "users#login"
   post "/logout", to: "users#logout", as: "logout"
   get "/users/current", to: "users#current", as: "current_user"
   
+  post "/vote/:work_id", to: "votes#create", as: "vote"
   
 end
