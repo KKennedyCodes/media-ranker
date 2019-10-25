8edb49893e5f5e86cb3ec81dc8b4b17a35e02a78
KKennedyCodes <holmstedtk@gmail.com>
2019-10-25 13:18:33 -0700

    Added Login/Logout to Navbar

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,7 @@ Rails.application.routes.draw do
   post "/vote/:work_id", to: "votes#create", as: "vote"
   
   get "/auth/github", as: "github_login"
-  get "/auth/:provider/callback", to: "users#create"
+  get "/auth/:provider/callback", to: "users#create", as: "auth_callback"
   delete "/logout", to: "users#destroy", as: "logout"
   
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -9,21 +9,38 @@ class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
   
+  def setup
+    OmniAuth.config_test_mode = true
+  end
+  
+  def mock_auth_hash(user)
+    return {
+      provider: user.provider,
+      uid: user.uid,
+      info: {
+        email: user.email,
+        name: user.name
+      }
+    }
+  end
+  
   # Add more helper methods to be used by all tests here...
   def perform_login(user = nil)
     user ||= User.first
     
     login_data = {
-    user: {
-    name: user.name,
-  },
-}
-
-post login_path, params: login_data
-
-expect(session[:user_id]).must_equal user.id
-
-return user
-end
-
+      user: {
+        name: user.name,
+      },
+    }
+    
+    post login_path, params: login_data
+    
+    expect(session[:user_id]).must_equal user.id
+    
+    return user
+  end
+  
+  
+  
 end
