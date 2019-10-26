4b3da40d5caf062a6466343ec9e88c8d50dddadd
KKennedyCodes <holmstedtk@gmail.com>
2019-10-25 23:29:21 -0700

    Added Basic Layout for Merchant Show

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,10 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 2019_10_21_181635) do
-
+  
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
+  
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.datetime "join_date"
@@ -24,7 +24,7 @@ ActiveRecord::Schema.define(version: 2019_10_21_181635) do
     t.string "provider"
     t.string "email"
   end
-
+  
   create_table "votes", force: :cascade do |t|
     t.datetime "date"
     t.datetime "created_at", null: false
@@ -34,7 +34,7 @@ ActiveRecord::Schema.define(version: 2019_10_21_181635) do
     t.index ["user_id"], name: "index_votes_on_user_id"
     t.index ["work_id"], name: "index_votes_on_work_id"
   end
-
+  
   create_table "works", force: :cascade do |t|
     t.string "category"
     t.string "title"
@@ -44,7 +44,7 @@ ActiveRecord::Schema.define(version: 2019_10_21_181635) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-
+  
   add_foreign_key "votes", "users"
   add_foreign_key "votes", "works"
 end
